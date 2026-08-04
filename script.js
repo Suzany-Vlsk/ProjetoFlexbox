ffa1577d8765d0a329aa8214019fe169b0a9bf19
Suzany <suzanyvaleska@outlook.com>
2026-08-03 21:33:56 -0300

    js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function alerta()
     alert("Você clicou em Ver Perfil!");
 }
 
+function mostrarPersonagem(event) {
+
+    event.preventDefault();
+
+    let personagem = document.getElementById("personagem").value;
+
+    let resposta = document.getElementById("resposta");
+
+    resposta.textContent = "Você escolheu " + personagem + " como seu personagem favorito! 🍥";
+}
