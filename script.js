4e40f12aade7ecf7bd61bc4a3cb4734d1a548cc2
Suzany <suzanyvaleska@outlook.com>
2026-08-03 21:44:49 -0300

    js + css

--- script.js
+++ script.js
@@ -8,8 +8,15 @@ function mostrarPersonagem(event) {
     event.preventDefault();
 
     let personagem = document.getElementById("personagem").value;
-
     let resposta = document.getElementById("resposta");
 
-    resposta.textContent = "Você escolheu " + personagem + " como seu personagem favorito! 🍥";
+    if (personagem.trim() === "") {
+        resposta.textContent = "Digite o nome de um personagem!";
+        return;
+    }
+
+    resposta.innerHTML = 
+        'Você escolheu <strong class="nome-personagem">' 
+        + personagem 
+        + '</strong> como seu personagem favorito! 🍥';
 }
