4f597efcfcc5ad97e2a780a367a465ac745e0116
Suzany <suzanyvaleska@outlook.com>
2026-08-05 21:33:49 -0300

    html + js + css

--- script.js
+++ script.js
@@ -7,27 +7,34 @@ function alerta()
 
 function mostrarCuriosidade() {
 
-    document.getElementById("textoCuriosidade").innerText =
-        "Curiosidade: Naruto Uzumaki foi o primeiro personagem criado por Masashi Kishimoto para a série.";
+    document.getElementById("textoCuriosidade").innerHTML = 
+    '<strong class="titulo-curiosidade">Curiosidade: </strong>Naruto Uzumaki foi o primeiro personagem criado por Masashi Kishimoto para a série.';
 
 }
 
 
 /* EXERCICO III */
+
 function mostrarPersonagem(event) {
 
-    event.preventDefault();
+    event.preventDefault(); /* Impede o formulário de ser enviado/recarregar a página para que o javacript possa mostrar a resposta diretamente na página */
 
     let personagem = document.getElementById("personagem").value;
     let resposta = document.getElementById("resposta");
-
-    if (personagem.trim() === "") {
-        resposta.textContent = "Digite o nome de um personagem!";
-        return;
-    }
-
-    resposta.innerHTML = 
-        'Você escolheu <strong class="nome-personagem">' 
-        + personagem 
-        + '</strong> como seu personagem favorito! 🍥';
+                                           /* <strong> serve para deixar nome do personagem em destaque */
+    resposta.innerHTML ='Você escolheu <strong class="nome-personagem">' + personagem + '</strong> como seu personagem favorito! 🍥';
 }
+
+/* EXERCICIO IV */
+
+function calcularPontos(){
+
+    let missoes = Number(document.getElementById("missoes").value);
+    let pontos = Number(document.getElementById("pontos").value);
+
+    let total = missoes * pontos;
+
+    document.getElementById("resultadoPontos").innerText =
+        "Seu ninja conquistou " + total + " pontos!";
+}
+
